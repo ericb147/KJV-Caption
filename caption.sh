50e1beec46f34924eb49620e46a61db328ce44e1
Eric Blankenship <ericblankenship147@gmail.com>
2023-09-18 15:47:35 -0400

    Fixed bug with for loop index.

--- caption.sh
+++ caption.sh
@@ -4,19 +4,17 @@ BOOK=$1
 CHAPTER=$2
 START=$3
 END=$4
-JPATH=./Bible-kjv/$1.JSON
-CINDEX=$(($2-1))
-VINDEX=$(($3-1))
+JPATH=./Bible-kjv/$BOOK.JSON
+CINDEX=$(($CHAPTER-1))
+VINDEX=$(($START-1))
 
 for i in $(eval echo {$START..$END})
 do
-    
-    VERSE=$(jq '.chapters | .['$CINDEX'].verses | .['$(($VINDEX+($i-1)))'].text' $JPATH)
+    VERSE=$(jq '.chapters | .['$CINDEX'].verses | .['$(($i-1))'].text' $JPATH)
     echo $VERSE
-    #scripture % jq '.chapters | .[0].verses | .[0].text' Genesis.json
-    echo "$BOOK $CHAPTER:$(($START+($i-1)))"
-    convert -background transparent -fill white -gravity center -size 1718x224 caption:"$VERSE" comtest.png +swap -gravity southeast -geometry +0+70 -composite output/"$BOOK"_"$CHAPTER"_"$(($START+($i-1)))".png
-    convert -background transparent -fill white -gravity center -size 530x80 caption:"$BOOK $CHAPTER:$(($START+($i-1)))" output/"$BOOK"_"$CHAPTER"_"$(($START+($i-1)))".png +swap -gravity southeast -composite output/"$BOOK"_"$CHAPTER"_"$(($START+($i-1)))".png
+    echo "$BOOK $CHAPTER:$(($i))"
+    convert -background transparent -fill white -gravity center -size 1718x224 caption:"$VERSE" comtest.png +swap -gravity southeast -geometry +0+70 -composite output/"$BOOK"_"$CHAPTER"_"$(($i))".png
+    convert -background transparent -fill white -gravity center -size 530x80 caption:"$BOOK $CHAPTER:$(($i))" output/"$BOOK"_"$CHAPTER"_"$(($i))".png +swap -gravity southeast -composite output/"$BOOK"_"$CHAPTER"_"$(($i))".png
 
 
 
